2d3bef34d05d04d3c575a7bd25f5504222d25838
persaianbot <mrx2121mrx@gmail.com>
2016-08-04 22:26:44 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '226453617:AAFBAR1OoNmLz-GYvtMrx5SMEehOUEhxzVA');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
